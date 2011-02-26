073c1352467e95a49c94261a5ede47a53882b4a8
Jon Bettinger <jonbettinger@gmail.com>
2011-02-25 22:34:13 -0500

    Refactor to all name tests at top level.

--- src/rose/Program.groovy
+++ src/rose/Program.groovy
@@ -1,84 +1,60 @@
 package rose
 
 class Program {
-
-void updateQuality()
-{
-	for (def i = 0; i < items.size(); i++)
-	{
-		if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-		{
-			if (items[i].quality > 0)
-			{
-				if (items[i].name != "Sulfuras, Hand of Ragnaros")
-				{
+	
+	void updateQuality() {
+		for (def i = 0; i < items.size(); i++) {
+			if (items[i].name == "Aged Brie") {
+				if (items[i].quality < 50) {
+					items[i].quality = items[i].quality + 1;
+				}
+			} else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+				if (items[i].quality < 50) {
+					items[i].quality = items[i].quality + 1;
+					
+					if (items[i].sellIn < 11) {
+						if (items[i].quality < 50) {
+							items[i].quality = items[i].quality + 1;
+						}
+					}
+					
+					if (items[i].sellIn < 6) {
+						if (items[i].quality < 50) {
+							items[i].quality = items[i].quality + 1;
+						}
+					}
+				}
+			} else if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+				if (items[i].quality > 0) {
 					items[i].quality = items[i].quality - 1;
 				}
 			}
-		}
-		else
-		{
-			if (items[i].quality < 50)
-			{
-				items[i].quality = items[i].quality + 1;
-
-				if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-				{
-					if (items[i].sellIn < 11)
-					{
-						if (items[i].quality < 50)
-						{
-							items[i].quality = items[i].quality + 1;
-						}
-					}
-
-					if (items[i].sellIn < 6)
-					{
-						if (items[i].quality < 50)
-						{
-							items[i].quality = items[i].quality + 1;
-						}
-					}
-				}
+			
+			if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+				items[i].sellIn = items[i].sellIn - 1;
 			}
-		}
-
-		if (items[i].name != "Sulfuras, Hand of Ragnaros")
-		{
-			items[i].sellIn = items[i].sellIn - 1;
-		}
-
-		if (items[i].sellIn < 0)
-		{
-			if (items[i].name != "Aged Brie")
-			{
-				if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-				{
-					if (items[i].quality > 0)
-					{
-						if (items[i].name != "Sulfuras, Hand of Ragnaros")
-						{
-							items[i].quality = items[i].quality - 1;
-						}
+			
+			if (items[i].name == "Aged Brie") {
+				if (items[i].sellIn < 0) {
+					if (items[i].quality < 50) {
+						items[i].quality = items[i].quality + 1;
 					}
 				}
-				else
-			   {
+			} else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+				if (items[i].sellIn < 0) {
 					items[i].quality = items[i].quality - items[i].quality;
 				}
-			}
-			else
-			{
-				if (items[i].quality < 50)
-			   {
-					items[i].quality = items[i].quality + 1;
+			} else if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+				if (items[i].sellIn < 0) {
+					if (items[i].quality > 0) {
+						items[i].quality = items[i].quality - 1;
+					}
 				}
 			}
 		}
 	}
-}
-
-List<Item> items = [
+	
+	List<Item> items = [
 		new Item ( name: "+5 Dexterity Vest", sellIn: 10, quality: 20 ),
 		new Item ( name: "Aged Brie", sellIn: 2, quality: 0 ),
 		new Item ( name: "Elixir of the Mongoose", sellIn: 5, quality: 7 ),
@@ -86,16 +62,13 @@ List<Item> items = [
 		new Item ( name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20 ),
 		new Item ( name: "Conjured Mana Cake", sellIn: 3, quality: 6 )
 	];
-
-
 }
 
-class Item
-{
+class Item {
 	String name
-
+	
 	int sellIn
-
+	
 	int quality
 	
 	String toString() {
